7d532ba67307e7003ceff22b0c8a553eccefdf86
med.amine <mohamed.amine.aloui@gmail.com>
2023-04-11 18:18:01 +0100

    feat: create factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(2)
+            ->hasLinks(5)
+            ->create();
+            
+        User::factory()->create();
+
+        User::factory()->create([
+            'role' => 'admin'
+        ]);
     }
 }
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,5 +16,17 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'login' => 'Log in',
+    'logout' => 'Log out',
+    'register' => 'Register',
+    'name' => 'Name',
+    'email' => 'E-mail',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'already_registered_q' => 'Already registred?',
+    'remember_me' => 'Remember me',
+    'forgot_your_password' => 'Forgot your password?',
+    'dashboard' => 'Dashboard',
+    'you_re_logged_in' => 'You\'re logged in !',
 
 ];
